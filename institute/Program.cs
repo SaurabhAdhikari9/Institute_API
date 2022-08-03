78f14c5ab05b6502ca00402cbe093635270367c5
Saurabh Adhikari <83704793+SaurabhAdhikari9@users.noreply.github.com>
2022-08-03 20:27:13 +0545

    Localstorage setup

--- institute/Program.cs
+++ institute/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 builder.Services.AddDbContext<StudentDbContext>(opt =>
 opt.UseInMemoryDatabase("Data"));
 
